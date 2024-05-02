fd2e8facd905b3bc8703333da6c5eec24ea9bf0f
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2024-05-02 16:28:29 +0300

    optimized main

--- src/Application.java
+++ src/Application.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         CommandExecutor ce = new CommandExecutor();
+        Scanner scanner = new Scanner(System.in);
+        String[] commands;
 
         System.out.println("Welcome! Use 'help' to see the list of commands\n");
 
         while (true) {
             try {
-                Scanner scanner = new Scanner(System.in);
-                String[] commands = scanner.nextLine().split(" ");
+                commands = scanner.nextLine().split(" ");
 
                 System.out.println();
                 ce.execute(commands);
