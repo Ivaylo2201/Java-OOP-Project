8ea4ba96a5010fd6ad2af723fb38685f87f65a1d
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2024-05-26 21:31:29 +0300

    Moved region maps

--- src/regions/Region.java
+++ src/regions/Region.java
@@ -12,7 +12,18 @@ import java.util.function.Function;
  * The Region abstract class represents a region in a graphical context.
  */
 public abstract class Region {
-    private final ProcessorMapper processorsMap = new ProcessorMapper();
+    private final Map<String, Function<Figure, List<String>>> propertiesMap = new HashMap<>();
+    private final Map<String, Function<List<String>, Boolean>> result = new HashMap<>();
+    private final ProcessorMapper processorsMapper = new ProcessorMapper();
+
+    public Region() {
+        this.propertiesMap.put("rectangle", (figure) -> this.processorsMapper.processors.get("rectangle").getProperties(figure));
+        this.propertiesMap.put("circle", (figure) -> this.processorsMapper.processors.get("circle").getProperties(figure));
+        this.propertiesMap.put("line", (figure) -> this.processorsMapper.processors.get("line").getProperties(figure));
+        this.result.put("rectangle", (properties) -> this.checkRectangle(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2)), Integer.parseInt(properties.get(3))));
+        this.result.put("circle", (properties) -> this.checkCircle(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2))));
+        this.result.put("line", (properties) -> this.checkLine(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2)), Integer.parseInt(properties.get(3))));
+    }
 
     /**
      * Checks if a figure is within the region.
@@ -21,18 +32,8 @@ public abstract class Region {
      * @return true if the figure is within the region, false otherwise.
      */
     public boolean isWithin(Figure figure) {
-        Map<String, Function<Void, List<String>>> propertiesMap = new HashMap<>();
-        propertiesMap.put("rectangle", _ -> this.processorsMap.processors.get("rectangle").getProperties(figure));
-        propertiesMap.put("circle", _ -> this.processorsMap.processors.get("circle").getProperties(figure));
-        propertiesMap.put("line", _ -> this.processorsMap.processors.get("line").getProperties(figure));
-
-        Map<String, Function<List<String>, Boolean>> result = new HashMap<>();
-        result.put("rectangle", (properties) -> this.checkRectangle(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2)), Integer.parseInt(properties.get(3))));
-        result.put("circle", (properties) -> this.checkCircle(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2))));
-        result.put("line", (properties) -> this.checkLine(Integer.parseInt(properties.getFirst()), Integer.parseInt(properties.get(1)), Integer.parseInt(properties.get(2)), Integer.parseInt(properties.get(3))));
-
         String figureType = figure.getClass().getSimpleName().toLowerCase();
-        List<String> figureProperties = propertiesMap.get(figureType).apply(null);
+        List<String> figureProperties = this.propertiesMap.get(figureType).apply(figure);
 
         return result.get(figureType).apply(figureProperties);
     }
